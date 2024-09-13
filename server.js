ba10309c5f8dbf430b56161118dffc57373e36ea
My Chau <nguyenlemychau1201@gmail.com>
2024-09-13 17:29:46 +0700

    Update registerAccount and loginAccount

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const cookieParser = require('cookie-parser');
 require("dotenv").config(); // Load các biến môi trường từ file .env
 
 // npx nodemon server.js // Chạy server
+
+app.use(express.json()); // Sử dụng middleware để phân tích JSON gửi đến từ client
+app.use(cors()); // Sử dụng middleware để xử lý lỗi CORS
+app.use(cookieParser()); // Sử dụng middleware để phân tích cookie từ client
 
 // Định nghĩa một route cơ bản
 app.get('/', (req, res) => {
@@ -16,9 +21,10 @@ app.listen(PORT, () => {
     console.log(`Server đang chạy trên port ${PORT}`);
 });
 
-app.use(express.json()); // Sử dụng middleware để phân tích JSON gửi đến từ client
-app.use(cors()); // Sử dụng middleware để xử lý lỗi CORS
-
 // Kết nối tới cơ sở dữ liệu
 require("./src/config/configMongoDB").connectDB(); // Kết nối với cơ sở dữ liệu MongoDB
+
+// Định tuyến cho các endpoints
+const authRoutes = require("./src/routes/authRoute"); // Import các routes cho music từ thư mục routes/music
+app.use("/", authRoutes); // Sử dụng routes cho user
 
