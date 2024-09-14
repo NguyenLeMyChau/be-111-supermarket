4a8bed3d5d76bb68bf5dbc681d509d7537f2a2cc
My Chau <nguyenlemychau1201@gmail.com>
2024-09-14 16:29:25 +0700

    Delete src

--- models/Account.js
+++ models/Account.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const accountSchema = new mongoose.Schema({
   phone: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true ,enum: ['staff', 'manager', 'customer'] }, // Chỉ định các vai trò
+  role: { type: String, required: true, enum: ['staff', 'manager', 'customer'] }, // Chỉ định các vai trò
   createdDate: { type: Date, default: Date.now },
   updateDate: { type: Date, default: Date.now },
-  active : {type:Boolean,default:true}
+  active: { type: Boolean, default: true }
 });
 
 const Account = mongoose.model('account', accountSchema, 'account');
