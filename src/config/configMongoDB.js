3a55de502943fdab119db5b41f29095804b43a2a
My Chau <nguyenlemychau1201@gmail.com>
2024-10-11 11:25:53 +0700

    fix connect Mongodb

--- src/config/configMongoDB.js
+++ src/config/configMongoDB.js
@@ -3,11 +3,7 @@ require('dotenv').config(); // Đọc biến môi trường từ .env
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000 // Tăng thời gian chờ kết nối lên 10 giây
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('MongoDB connection error:', err);
