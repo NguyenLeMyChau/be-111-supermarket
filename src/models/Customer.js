ef200a6f0e24f2bd890f80248474e6fe26b69f82
My Chau <nguyenlemychau1201@gmail.com>
2024-10-16 16:34:40 +0700

    commit object address customer

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
+const addressSchema = new mongoose.Schema({
+    city: { type: String, trim: true },
+    district: { type: String, trim: true },
+    ward: { type: String, trim: true },
+    street: { type: String, trim: true }
+});
+
 const customerSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
-    address: { type: String, required: true, trim: true },
+    address: { type: addressSchema },
     phone: { type: String, required: true, unique: true, trim: true },
-    email: { type: String, required: true, unique: true, trim: true },
-    gender: { type: Boolean, trim: true }, //False: Nam, True: Nữ,
+    email: { type: String, unique: true, trim: true },
+    gender: { type: Boolean, trim: true }, // False: Nam, True: Nữ
     loyaltyPoints: { type: Number, default: 0 },
     barcode: { type: String, required: true, unique: true, trim: true },
     account_id: { type: mongoose.Schema.Types.ObjectId, ref: 'account' }
