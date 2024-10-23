beafc229a3f972dd76fa8bd16195e635155ff715
My Chau <nguyenlemychau1201@gmail.com>
2024-10-23 23:22:24 +0700

    allow null for price in product in cart

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -8,8 +8,8 @@ const cartSchema = new mongoose.Schema({
             _id: false,
             product_id: { type: mongoose.Schema.Types.ObjectId, ref: 'product', required: true },
             quantity: { type: Number, required: true, default: 1 },
-            price: { type: mongoose.Schema.Types.ObjectId, ref: 'productPrice_detail', required: true  },
-            total:{type :Number,required: true,default:1}
+            price: { type: mongoose.Schema.Types.ObjectId, ref: 'productPrice_detail' },
+            total: { type: Number, required: true, default: 1 }
         }
     ]
 }, {
