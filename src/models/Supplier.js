596860cb6ad6a3bb65bd69c0f99c4a120ea42a91
My Chau <nguyenlemychau1201@gmail.com>
2024-11-08 21:40:31 +0700

    change supplier address

--- src/models/Supplier.js
+++ src/models/Supplier.js
@@ -2,9 +2,16 @@ const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 const Schema = mongoose.Schema;
 
+const addressSchema = new mongoose.Schema({
+  city: { type: String, trim: true },
+  district: { type: String, trim: true },
+  ward: { type: String, trim: true },
+  street: { type: String, trim: true }
+});
+
 const supplierSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
-  address: { type: String, required: true, trim: true },
+  address: { type: addressSchema },
   phone: { type: String, required: true, unique: true, trim: true },
   email: { type: String, required: true, unique: true, trim: true },
   description: { type: String, required: true, trim: true },
